71925f7bd4d4be0eed7811de71174cb2f95136ba
bfourgeaud <benjamin.fourgeaud@gmail.com>
2021-03-30 17:52:01 +0200

    change build dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cosygames.jeebie.me',
+    title: 'Cosy Games',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -79,6 +79,8 @@ export default {
       clientsClaim: false
     }
   },
+
+  buildDir: 'dist',
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
